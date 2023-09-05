06709a526718922f4091efb1aee54b80a9f37995
damerri702 <124094631+damerri702@users.noreply.github.com>
2023-09-04 18:17:27 -0700

    Create lab2pt2.py

---
+++ lab2pt2.py
@@ -0,0 +1,5 @@
+DAYS_IN_YEAR = 365
+MINUTES_IN_HOUR = 60
+minutes_in_year = DAYS_IN_YEAR * 24 * MINUTES_IN_HOUR
+print("The number of minutes in a year is:", minutes_in_year)
+print("My name is Daniel Merrill")
